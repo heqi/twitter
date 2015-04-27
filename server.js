18dceac83c0018af8e089481ff07600d0984c5d7
heqi <heqi37@gmail.com>
2015-04-27 08:46:04 -0500

    nodejs server link to twitter api

--- server.js
+++ server.js
@@ -4,11 +4,137 @@ var morgan = require('morgan');
 var app = express();
 var async = require("async");
 
+var Twitter = require('twitter');
+var config = require('./config');
+var client = new Twitter(config.twitter);
+
 app.use(morgan('dev'));
 app.use(gzippo.staticGzip("" + __dirname + "/dist"));
 
-app.get('/api',function(req, res) {
-	 res.send("hello api");
+var twitterApi = function(url, params, callback, saveResult)
+{
+  client.get(url, params, function(error, tweets, response){
+	  if (!error) {
+	  	saveResult(tweets);
+	    callback();
+	  } else
+	  {
+	  	console.log(error);
+	  	callback(error);
+	  }
+	});
+}
+
+var twitterFollowersIds = function(value, callback, saveResult)
+{
+	var url = 'followers/ids';
+	var params = {screen_name:value};
+	twitterApi(url, params, callback, saveResult);
+}
+
+var twitterFollowingsIds = function(value, callback, saveResult)
+{
+	var url = 'friends/ids';
+	var params = {screen_name:value};
+	twitterApi(url, params, callback, saveResult);
+}
+
+var twitterFollowersList = function(value, callback, saveResult)
+{
+	var url = 'followers/list';
+	var params = {user_id:value};
+	twitterApi(url, params, callback, saveResult);
+}
+
+var twitterFollowingsList = function(value, callback, saveResult)
+{
+	var url = 'friends/list';
+	var params = {user_id:value};
+	twitterApi(url, params, callback, saveResult);
+}
+
+var twitterUsersLookup = function(value, callback, saveResult)
+{
+	var url = 'users/lookup';
+	var params = {user_id:value};
+	twitterApi(url, params, callback, saveResult);
+}
+
+var compareArray = function(array1, array2) {
+	var result = [];
+	for (var i = 0; i < array1.length; i++) {
+		var user1 = array1[i];
+		for (var j = 0; j < array2.length; j++) {
+			var user2 = array2[j];
+			if (user1 == user2)
+			{
+				result.push(user1);				
+			}
+		}
+	}
+	return result;
+}
+
+app.get('/api/twitter/search',function(req, res) {
+	var firstUsername = req.query.firstUsername;
+	var secondUsername = req.query.secondUsername;
+	var result = {};
+	var tmpResult = {};
+
+	if (firstUsername == null || secondUsername == null) {
+		console.log("error:wrong params");
+		res.status(500).send("error: user not exist");
+	  return;
+	}
+
+	async.parallel([
+    function(callback) {
+   		twitterFollowersIds(firstUsername, callback, function(value) {tmpResult.firstFollowers = value;});
+    },
+    function(callback) {
+    	twitterFollowersIds(secondUsername, callback, function(value) {tmpResult.secondFollowers = value;});
+    },
+    function(callback) {
+   		twitterFollowingsIds(firstUsername, callback, function(value) {tmpResult.firstFollowings = value;});
+    },
+    function(callback) {
+    	twitterFollowingsIds(secondUsername, callback, function(value) {tmpResult.secondFollowings = value;});
+    }
+	], function(err) {
+	    if (err) return res.status(500).send(err);
+
+	    if (
+	    	tmpResult.firstFollowers == null ||
+	    	tmpResult.secondFollowers == null ||
+	    	tmpResult.firstFollowers.ids == null ||
+	    	tmpResult.secondFollowers.ids == null ||
+	    	tmpResult.firstFollowings == null ||
+	    	tmpResult.secondFollowings == null ||
+	    	tmpResult.firstFollowings.ids == null ||
+	    	tmpResult.secondFollowings.ids == null) {
+	    	console.log("error:wrong ids result");
+	    	res.status(500).send("error: user not exist");
+	    	return;
+	    }
+
+	    result.followersIds = compareArray(tmpResult.firstFollowers.ids, tmpResult.secondFollowers.ids);
+	    result.followingsIds = compareArray(tmpResult.firstFollowings.ids, tmpResult.secondFollowings.ids);
+
+	    async.parallel([
+		    function(callback2) {
+		   		twitterUsersLookup(result.followersIds.join(","), callback2, function(value) {result.followersList = value;});
+		    },
+		    function(callback2) {
+		    	twitterUsersLookup(result.followingsIds.join(","), callback2, function(value) {result.followingsList = value;});
+		    }
+			], function(err) {
+				if (err) return res.status(500).send(err);
+
+				res.send(result);
+
+			});
+	});
 })
+
 
 app.listen(process.env.PORT || 5000);
---
+++ src/config.js
@@ -0,0 +1,12 @@
+var config = {};
+config.web = {};
+config.twitter = {};
+
+config.twitter.consumer_key = "vi7qs7L1ggUqpWS0M1NY3txAq";
+config.twitter.consumer_secret = "k4MtgkbW4dleXbMdcpjhxL7RTYvf2FULECJCKZUXAC5vbJeDTs";
+config.twitter.access_token_key = "340366357-BpalCotT5KyWsjFpzh2Vd2mdm5TYBpPAUEJpI9dT";
+config.twitter.access_token_secret = "eQ1qonJoFO6JNNLBJj12Hb6Gelf7XCQK4yQgScRv32JaU";
+
+config.web.port = process.env.PORT || 5000;
+
+module.exports = config;
